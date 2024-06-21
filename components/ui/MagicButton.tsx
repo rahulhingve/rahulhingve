fc4c25e0da242022dfa19998a49083648cdcb9eb
unknown <rahulpawar2001.rp@gmail.com>
2024-06-21 23:19:31 +0530

    Ad new Animation buttion . old not rendering in moile

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -7,9 +7,9 @@ const MagicButton = ({
 }) => {
     return (
 
-        <button className="relative inline-flex h-12 w-full overflow-hidden rounded-lg p-[1px] focus:outline-none md:w-60 md:mt-10 " onClick={handleClick}>
-            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]"  />
-            <span className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 py-1 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}>
+        <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50" onClick={handleClick}>
+    
+            <span className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg  px-7 py-1 text-sm font-medium text-white  gap-2 ${otherClasses}`}>
                 {position === 'left' && icon}
 
                 {title}
@@ -25,3 +25,24 @@ const MagicButton = ({
 
 export default MagicButton
 
+
+        // Button code
+        
+  
+        // tailwind.config.js code
+        // {
+        //   "animation": {
+        //     shimmer: "shimmer 2s linear infinite"
+        //   },
+        //   "keyframes": {
+        //     shimmer: {
+        //       from: {
+        //         "backgroundPosition": "0 0"
+        //       },
+        //       to: {
+        //         "backgroundPosition": "-200% 0"
+        //       }
+        //     }
+        //   }
+        // }
+      
