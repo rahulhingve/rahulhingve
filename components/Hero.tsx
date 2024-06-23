e100770b574e6fe10d03c2084e6f1f247ddb2893
unknown <rahulpawar2001.rp@gmail.com>
2024-06-23 17:41:28 +0530

    updated hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,13 +11,13 @@ const Hero = () => {
             <div>
                 <Spotlight
                     className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-                    fill="white"
+                    fill="pink"
                 />
                 <Spotlight
                     className="h-[80vh] w-[50vw] top-10 left-full"
                     fill="purple"
                 />
-                <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+                <Spotlight className="left-80 top-28 h-[90vh] w-[50vw]" fill="blue" />
             </div>
             <div className="h-screen w-full bg-black-100   bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center">
@@ -29,11 +29,11 @@ const Hero = () => {
             <div className='flex justify-center relative my-20 z-10'>
                 <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center '>
                     <h2 className="uppercase tracking-widest text-xs text-center  max-w-80 text-white">
-                        Dynamic Web Magic with Next.js
+                        Made with Next.js
                     </h2>
                     <TextGenerateEffect
                         className="text-center text-[40px] md:text-5xl lg:text-6xl"
-                        words="yooo hi my name is rahul "
+                        words="Innovating User Journeys from Concept to Reality."
                     />
                     <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl text-white '>
                         Hi, I&apos;m Rahul , A full-stack Developer based in India
